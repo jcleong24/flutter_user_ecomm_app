896e6f6b61d6c09fde03031240f5ec76f7037035
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-03-01 01:39:28 +0800

    intergrate router screen

--- lib/core/routers/app_router.dart
+++ lib/core/routers/app_router.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_user_ecomm_app/core/routers/route_name.dart';
 import 'package:go_router/go_router.dart';
+import '../../screens/cart_screen.dart';
+import '../../screens/checkout_screen.dart';
+import '../../screens/home_screen.dart';
+import '../../screens/payment_screen.dart';
+import '../../screens/product_details_screen.dart';
 import '../../screens/splash_screen.dart';
 
 class AppRouter {
@@ -9,6 +14,26 @@ class AppRouter {
       GoRoute(
         path: RouteNames.splash,
         builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: RouteNames.home,
+        builder: (context, state) => const ProductListScreen(),
+      ),
+      GoRoute(
+        path: RouteNames.productDetails,
+        builder: (context, state) => const ProductDetailsScreen(),
+      ),
+      GoRoute(
+        path: RouteNames.cart,
+        builder: (context, state) => const CartScreen(),
+      ),
+      GoRoute(
+        path: RouteNames.checkout,
+        builder: (context, state) => const CheckoutScreen(),
+      ),
+      GoRoute(
+        path: RouteNames.payment,
+        builder: (context, state) => const PaymentScreen(),
       ),
     ],
   );
