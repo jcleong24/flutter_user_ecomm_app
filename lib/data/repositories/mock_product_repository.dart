48e71c31e13eaea09c8c5373de6b4bdd943def45
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-03-02 22:11:33 +0800

    removed mock_product_repository

--- lib/data/repositories/mock_product_repository.dart
+++ lib/data/repositories/mock_product_repository.dart
@@ -1,36 +1,42 @@
-import '../../domain/models/product.dart';
-import 'product_repository.dart';
+// import '../../domain/models/product.dart';
+// import 'product_repository.dart';
 
-class MockProductRepository implements ProductRepository {
-  @override
-  Future<List<Product>> fetchProducts() async {
-    await Future.delayed(const Duration(seconds: 1));
+// class MockProductRepository implements ProductRepository {
+//   @override
+//   Future<List<Product>> fetchProducts() async {
+//     await Future.delayed(const Duration(seconds: 1));
 
-    return const [
-      Product(
-        id: '101',
-        name: 'Nike Air Max',
-        description: 'Comfortable running shoes',
-        price: 399.0,
-        stockQty: 12,
-        imageUrl: 'https://picsum.photos/300?1',
-      ),
-      Product(
-        id: '102',
-        name: 'Leather Bag',
-        description: 'Premium daily carry bag',
-        price: 259.0,
-        stockQty: 5,
-        imageUrl: 'https://picsum.photos/300?2',
-      ),
-      Product(
-        id: '103',
-        name: 'Running Shoes',
-        description: 'Lightweight running shoes',
-        price: 499.0,
-        stockQty: 8,
-        imageUrl: 'https://picsum.photos/300?3',
-      ),
-    ];
-  }
-}
+//     return const [
+//       Product(
+//         id: '101',
+//         name: 'Nike Air Max',
+//         description: 'Comfortable running shoes',
+//         price: 399.0,
+//         stockQty: 12,
+//         imageUrl: 'https://picsum.photos/300?1',
+//       ),
+//       Product(
+//         id: '102',
+//         name: 'Leather Bag',
+//         description: 'Premium daily carry bag',
+//         price: 259.0,
+//         stockQty: 5,
+//         imageUrl: 'https://picsum.photos/300?2',
+//       ),
+//       Product(
+//         id: '103',
+//         name: 'Running Shoes',
+//         description: 'Lightweight running shoes',
+//         price: 499.0,
+//         stockQty: 8,
+//         imageUrl: 'https://picsum.photos/300?3',
+//       ),
+//     ];
+//   }
+  
+//   @override
+//   Future<Product> fetchProductById(String id) {
+//     // TODO: implement fetchProductById
+//     throw UnimplementedError();
+//   }
+// }
