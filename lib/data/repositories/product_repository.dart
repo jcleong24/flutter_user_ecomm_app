dbae64921cdaec89ca8b9ce5436b2fcb6b0056e9
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-03-02 18:06:31 +0800

    add firebase product_repository

--- lib/data/repositories/product_repository.dart
+++ lib/data/repositories/product_repository.dart
@@ -1,5 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../../domain/models/product.dart';
 
 abstract class ProductRepository {
   Future<List<Product>> fetchProducts();
+}
+
+class FirebaseProductRepository implements ProductRepository {
+  final FirebaseFirestore firestore;
+
+  FirebaseProductRepository(this.firestore);
+
+  @override
+  Future<List<Product>> fetchProducts() async {
+    final snapshot = await firestore
+        .collection('products')
+        .orderBy('createdAt', descending: true)
+        .get();
+    return snapshot.docs.map(Product.fromFirestore).toList();
+  }
 }
