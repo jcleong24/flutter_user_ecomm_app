de991e367b1c8922bd4dd0477245f455c04da4aa
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-03-04 15:27:18 +0800

    add order_bloc

---
+++ lib/domain/bloc/order/order_event.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_user_ecomm_app/domain/models/cart_item.dart';
+
+abstract class OrderEvent extends Equatable {
+  const OrderEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class OrderPlaced extends OrderEvent {
+  final List<CartItem> cartItems;
+
+  const OrderPlaced(this.cartItems);
+
+  @override
+  List<Object?> get props => [cartItems];
+}
+
+class OrderReset extends OrderEvent {
+  const OrderReset();
+}
