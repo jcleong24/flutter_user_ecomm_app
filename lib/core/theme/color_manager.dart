743dff8a4458967293368907f4f2e0191fcec8c3
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-03-07 21:30:21 +0800

    added more color style

--- lib/core/theme/color_manager.dart
+++ lib/core/theme/color_manager.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   // Brand
-  static const Color primary = Color(0xFFFF7622);
+  static const Color primary = Color(0xCCFF7622);
+  static const Color primaryVariant = Color(0xFF7C3AED);
+  static const Color primaryTransparent = Color(0x99FF7622);
   static const Color secondary = Color(0xFFFFD1B4);
 
   // Background
@@ -11,10 +13,21 @@ class ColorManager {
   static const Color textPrimary = Color(0xCC111827);
   static const Color textSecondary = Color(0xCC6B7280);
   static const Color border = Color(0xCCE5E7EB);
+  static const Color borderTransparent = Color(0x2EFFFFFF);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color whiteTransparent = Color(0xE5FFFFFF);
   // static Color border = Colors.white.withOpacity(0.16);
 
   // Status
   static const Color success = Color(0xFF16A34A);
   static const Color warning = Color(0xFFF59E0B);
   static const Color error = Color(0xFFDC2626);
+
+  // static const Color border100 = Color(0xFFE5E7EB); // 100%
+  // static const Color border80 = Color(0xCCE5E7EB); // 80%
+  // static const Color border60 = Color(0x99E5E7EB); // 60%
+  // static const Color border50 = Color(0x80E5E7EB); // 50%
+  // static const Color border30 = Color(0x4DE5E7EB); // 30%
+  // static const Color border20 = Color(0x33E5E7EB); // 20%
 }
